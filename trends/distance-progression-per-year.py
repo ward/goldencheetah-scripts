088d518eb501186d96674d1ed420c91eeba4ec42
Ward Muylaert <ward.muylaert@gmail.com>
2020-04-27 21:09:52 +0200

    Run black formatter on all

--- trends/distance-progression-per-year.py
+++ trends/distance-progression-per-year.py
@@ -4,12 +4,15 @@ import pathlib
 
 GOAL_DISTANCE = 4000
 
+
 def is_leap_year(year):
     return (year % 4 == 0 and year % 100 != 0) or year % 400 == 0
+
 
 def goal_progression(goal, steps):
     step = goal / steps
     return [i * step for i in range(steps + 1)]
+
 
 ###############################################################################
 ####### Calculate yearly progression ##########################################
@@ -17,13 +20,15 @@ def goal_progression(goal, steps):
 
 # Cannot use their filter as it is terribly slow (30 seconds or so)
 # Instead, filter afterwards
-full_metrics = GC.seasonMetrics(all=True, filter='')
-relevant_metrics = zip(full_metrics['date'], full_metrics['Distance'], full_metrics['Sport'])
-metrics = list(filter(lambda threetuple: threetuple[2] == 'Run', relevant_metrics))
+full_metrics = GC.seasonMetrics(all=True, filter="")
+relevant_metrics = zip(
+    full_metrics["date"], full_metrics["Distance"], full_metrics["Sport"]
+)
+metrics = list(filter(lambda threetuple: threetuple[2] == "Run", relevant_metrics))
 
 today = datetime.date.today()
-start = datetime.date(year = full_metrics['date'][0].year, month = 1, day = 1)
-a_day = datetime.timedelta(days = 1)
+start = datetime.date(year=full_metrics["date"][0].year, month=1, day=1)
+a_day = datetime.timedelta(days=1)
 
 distances = {}
 while start <= today:
@@ -57,14 +62,23 @@ for year in distances:
 import plotly
 import plotly.graph_objs as go
 
-data = [go.Scatter(x = list(range(366)), y = distances[year], mode = 'lines', name = year) for year in distances]
-data.append(go.Scatter(x = list(range(366)), y = goal_progression(GOAL_DISTANCE, 365), mode = 'lines', name = 'Goal'))
+data = [
+    go.Scatter(x=list(range(366)), y=distances[year], mode="lines", name=year)
+    for year in distances
+]
+data.append(
+    go.Scatter(
+        x=list(range(366)),
+        y=goal_progression(GOAL_DISTANCE, 365),
+        mode="lines",
+        name="Goal",
+    )
+)
 
-temp_file = tempfile.NamedTemporaryFile(mode="w+t", prefix="GC_", suffix=".html", delete=False)
+temp_file = tempfile.NamedTemporaryFile(
+    mode="w+t", prefix="GC_", suffix=".html", delete=False
+)
 
-f = plotly.offline.plot({
-    'data': data
-}, auto_open = False, filename=temp_file.name)
+f = plotly.offline.plot({"data": data}, auto_open=False, filename=temp_file.name)
 GC.webpage(pathlib.Path(f).as_uri())
 print("DEBUG: plot saved in", temp_file.name)
-
--- trends/year-distance-goal-text.py
+++ trends/year-distance-goal-text.py
@@ -8,12 +8,12 @@ GOAL = 4500
 ### Calculations
 data = GC.seasonMetrics()
 # Keep only runs. Don't use GC filters cause that makes it HELLA slow
-distances = [x for i, x in enumerate(data['Distance']) if data['Sport'][i] == 'Run']
+distances = [x for i, x in enumerate(data["Distance"]) if data["Sport"][i] == "Run"]
 
 today_distance = sum(distances)
 today = datetime.date.today()
-first_day = datetime.date(year = today.year, month = 1, day = 1)
-last_day = datetime.date(year = today.year, month = 12, day = 31)
+first_day = datetime.date(year=today.year, month=1, day=1)
+last_day = datetime.date(year=today.year, month=12, day=31)
 days_passed = (today - first_day).days + 1
 days_to_go = (last_day - today).days
 total_days = (last_day - first_day).days + 1
@@ -24,28 +24,40 @@ last_distance_estimate = km_per_day * total_days
 today_goal = GOAL / total_days * days_passed
 
 ### Create html file
-temp_file = tempfile.NamedTemporaryFile(mode="w+t", prefix="GC_", suffix=".html", delete=False)
-temp_file.write('<html><body>')
-temp_file.write('<p>')
+temp_file = tempfile.NamedTemporaryFile(
+    mode="w+t", prefix="GC_", suffix=".html", delete=False
+)
+temp_file.write("<html><body>")
+temp_file.write("<p>")
 temp_file.write("You have run {:.1f} km so far.".format(today_distance))
-temp_file.write('</p>')
-temp_file.write('<p>')
+temp_file.write("</p>")
+temp_file.write("<p>")
 temp_file.write("That is an average of {:.1f} km per day.".format(km_per_day))
-temp_file.write('</p>')
+temp_file.write("</p>")
 
-temp_file.write('<p>')
+temp_file.write("<p>")
 if today_goal < today_distance:
-    temp_file.write("You are {:.1f} km ahead of your {:d} km goal.".format(today_distance - today_goal, GOAL))
+    temp_file.write(
+        "You are {:.1f} km ahead of your {:d} km goal.".format(
+            today_distance - today_goal, GOAL
+        )
+    )
 else:
-    temp_file.write("You are {:.1f} km BEHIND on your {:d} km goal.".format(today_goal - today_distance, GOAL))
-temp_file.write('</p>')
+    temp_file.write(
+        "You are {:.1f} km BEHIND on your {:d} km goal.".format(
+            today_goal - today_distance, GOAL
+        )
+    )
+temp_file.write("</p>")
 
-temp_file.write('<p>')
-temp_file.write("At this rate, you will end up at {:.1f} km.".format(last_distance_estimate))
-temp_file.write('</p>')
-temp_file.write('</body></html>')
+temp_file.write("<p>")
+temp_file.write(
+    "At this rate, you will end up at {:.1f} km.".format(last_distance_estimate)
+)
+temp_file.write("</p>")
+temp_file.write("</body></html>")
 temp_file.close()
 
 ### Output
-print('DEBUG: File saved at', pathlib.Path(temp_file.name).as_uri())
+print("DEBUG: File saved at", pathlib.Path(temp_file.name).as_uri())
 GC.webpage(pathlib.Path(temp_file.name).as_uri())
