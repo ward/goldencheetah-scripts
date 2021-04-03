b870fa2188dfbec59c1a705608b3df4d2d37a105
Ward Muylaert <ward.muylaert@gmail.com>
2021-04-03 14:51:41 +0200

    Show different rolling total ranges
    Just the four I had before:
    
    - 1 week
    - 4 weeks
    - 12 weeks
    - 365 days

--- trends/rolling-total.py
+++ trends/rolling-total.py
@@ -5,7 +5,7 @@ import pathlib
 import matplotlib.pyplot as plt
 from io import BytesIO
 
-DAYS = 7
+DAYS = [7, 28, 84, 365]
 
 
 def days_range(start_day, end_day):
@@ -20,6 +20,31 @@ def selected_days():
     return days_range(data["date"][0], data["date"][-1])
 
 
+def calculate_rolling_total(number_of_days):
+    rolling_per_day = {}
+    for d in days_for_analysis:
+        old_day = max(d - datetime.timedelta(days=number_of_days - 1), all_days[0])
+        range_of_interest = days_range(old_day, d)
+        total = 0
+        for interesting_day in range_of_interest:
+            total += per_day[interesting_day]
+        rolling_per_day[d] = total
+    return rolling_per_day
+
+
+def get_rolling_svg(rolling):
+    fig, ax = plt.subplots()
+    ax.plot(days_for_analysis, [rolling[d] for d in days_for_analysis])
+
+    tmpfile = BytesIO()
+    fig.savefig(tmpfile, format="svg")
+    svg = tmpfile.getvalue().decode("utf-8")
+    # Release memory
+    plt.close(fig)
+
+    return svg
+
+
 DATA = GC.seasonMetrics(all=True)
 all_days = days_range(DATA["date"][0], DATA["date"][-1])
 per_day = {}
@@ -30,26 +55,12 @@ for i in range(len(DATA["Distance"])):
         per_day[DATA["date"][i]] += DATA["Distance"][i]
 
 days_for_analysis = selected_days()
-rolling_per_day = {}
-for d in days_for_analysis:
-    old_day = max(d - datetime.timedelta(days=DAYS - 1), all_days[0])
-    range_of_interest = days_range(old_day, d)
-    total = 0
-    for interesting_day in range_of_interest:
-        total += per_day[interesting_day]
-    rolling_per_day[d] = total
+rolling_per_day = map(calculate_rolling_total, DAYS)
 
-fig, ax = plt.subplots()
-ax.plot(days_for_analysis, [rolling_per_day[d] for d in days_for_analysis])
-
-tmpfile = BytesIO()
-fig.savefig(tmpfile, format="svg")
-html = "<html><body>" + tmpfile.getvalue().decode("utf-8") + "</body></html>"
-# Release memory
-plt.close(fig)
+svgs = (chr(10)).join(map(get_rolling_svg, rolling_per_day))
+html = "<html><body>" + svgs + "</body></html>"
 
 NAME = "/tmp/gc-rolling-total.html"
 with open(NAME, "w") as f:
     f.write(html)
-print(NAME)
 GC.webpage(pathlib.Path(NAME).as_uri())
