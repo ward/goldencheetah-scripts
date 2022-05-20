bb9153d999076f404d6595d67bbe30129a7dfb1e
Ward Muylaert <ward.muylaert@gmail.com>
2022-05-20 09:49:50 +0200

    Add nix' default.nix
    Use with `nix-shell`.

---
+++ headless/default.nix
@@ -0,0 +1,13 @@
+{ pkgs ? import <nixos> {} }:
+
+# Run nix-shell in this directory and it will pick up on default.nix and
+# provide python + the packages required.
+
+pkgs.mkShell {
+  buildInputs = [
+    pkgs.python39
+    pkgs.python39Packages.matplotlib
+    # Autoformatting code
+    pkgs.black
+  ];
+}
