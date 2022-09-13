da6a61c1c35c55253d432ec7ebeedebca8f9b597
Ward Muylaert <ward.muylaert@gmail.com>
2022-09-13 21:37:37 +0200

    Also show month and year distance bar charts

--- headless/distancepertime.py
+++ headless/distancepertime.py
@@ -5,8 +5,32 @@ from io import BytesIO
 
 import goldencheetah
 
-runs = goldencheetah.get_all_activities(sport="Run")
-runs = goldencheetah.group_by_week(runs)
+
+def sum_per_month(list_of_runs):
+    """Every run is an Activity, just start counting distance. Not very generic evidently."""
+    months = {}
+    for run in runs:
+        month = "{}-{}".format(run.date.year, run.date.month)
+        try:
+            months[month] += run.distance
+        except KeyError:
+            months[month] = run.distance
+    return months
+
+
+def sum_per_year(list_of_runs):
+    per_year = {}
+    for run in runs:
+        # Turning to string for consistency with the other summing.
+        # Specifically it otherwise confuses matplotlib's bar coordinates.
+        # The string one would go 0..len()-1, while this otherwise would go
+        # lowest_year..highest_year
+        year = str(run.date.year)
+        try:
+            per_year[year] += run.distance
+        except KeyError:
+            per_year[year] = run.distance
+    return per_year
 
 
 def sum_week(week_of_runs):
@@ -17,6 +41,11 @@ def sum_week(week_of_runs):
     return sum
 
 
+runs = goldencheetah.get_all_activities(sport="Run")
+per_month = sum_per_month(runs)
+per_year = sum_per_year(runs)
+runs = goldencheetah.group_by_week(runs)
+
 for week in runs.keys():
     runs[week] = sum_week(runs[week])
 
@@ -25,14 +54,26 @@ for week in runs.keys():
 # Values: Distance summed up
 
 
-def create_svg(distance_per_week):
+def create_svg(distance_per_time, timeframe):
     fig, ax = plt.subplots()
-    weeks = list(distance_per_week.keys())[-20:]
-    ax.bar(weeks, [distance_per_week[week] for week in weeks])
-    ax.grid(axis='y')
+    time_units = list(distance_per_time.keys())[-20:]
+    distances = [distance_per_time[time_unit] for time_unit in time_units]
+    ax.bar(time_units, distances)
+
+    # Put values on the bars
+    for i in range(len(time_units)):
+        ax.text(
+            i,
+            distances[i],
+            int(distances[i]),
+            horizontalalignment="center",
+            fontsize="small",
+        )
+
+    ax.grid(axis="y")
     ax.set_xlabel("Date")
     ax.set_ylabel("Distance (km)")
-    ax.set_title("Distance per week")
+    ax.set_title("Distance per {}".format(timeframe))
     plt.xticks(rotation=45)
     # ax.minorticks_on()
 
@@ -55,7 +96,9 @@ html = (
     + '<head><meta charset="utf-8" />'
     + "<title>Distance per week</title>"
     + "</head><body>"
-    + create_svg(runs)
+    + create_svg(runs, "week")
+    + create_svg(per_month, "month")
+    + create_svg(per_year, "year")
     + "<footer><p>Generated on {}.</p></footer>".format(now)
     + "</body></html>"
 )
