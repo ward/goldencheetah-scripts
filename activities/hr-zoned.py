0a26ce1a5bb2c4fdd31872bb4d313a3ac79cfdd5
Ward Muylaert <ward.muylaert@gmail.com>
2025-08-17 19:02:43 -0400

    Add in-gc activity HR view
    With all the changes to user charts etc, my old stuff no longer works
    and figuring out their entire weird language is too much effort. So just
    back to Python scripts. They're still a little messy, but easier to
    figure out.

---
+++ activities/hr-zoned.py
@@ -0,0 +1,84 @@
+import plotly
+import plotly.graph_objs as go
+import math
+import tempfile
+import pathlib
+
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from typing import Any
+
+    class GC:
+        @classmethod
+        def activity(cls) -> Any: ...
+        @classmethod
+        def webpage(cls, _: str) -> Any: ...
+
+
+def secs_to_hms(s):
+    if math.isnan(s):
+        return s
+    s = math.floor(s)
+    h = math.floor(s / 3600)
+    s = s % 3600
+    m = math.floor(s / 60)
+    s = s % 60
+    if h > 0:
+        return "{}:{:0>2}:{:0>2}".format(h, m, s)
+    else:
+        return "{}:{:0>2}".format(m, s)
+
+
+heartrates_map = {
+    int(s): h for s, h in zip(GC.activity()["seconds"], GC.activity()["heart.rate"])
+}
+seconds = list(range(max(heartrates_map.keys())))
+heartrates = [heartrates_map.get(s) for s in seconds]
+hovertext = []
+
+
+for hr, sec in zip(heartrates, seconds):
+    s = secs_to_hms(sec)
+    hovertext.append(f"({s}, {hr})")
+
+
+###########################################
+# Actual plotting
+temp_file = tempfile.NamedTemporaryFile(
+    mode="w+t", prefix="GC_", suffix=".html", delete=False
+)
+f = plotly.offline.plot(
+    {
+        "data": [
+            go.Scatter(
+                x=seconds,
+                y=heartrates,
+                name="HR",
+                text=hovertext,
+                hoverinfo="text",
+                line=dict(color="red"),
+            ),
+        ],
+        "layout": go.Layout(
+            yaxis=dict(range=[120, 190]),
+            shapes=[
+                dict(
+                    type="line",
+                    xref="paper",
+                    x0=0,
+                    x1=1,
+                    yref="y",
+                    y0=y,
+                    y1=y,
+                    line=dict(color="#333333", dash="dash"),
+                )
+                for y in [144, 154, 170, 190]
+            ],
+        ),
+    },
+    auto_open=False,
+    filename=temp_file.name,
+)
+
+GC.webpage(pathlib.Path(f).as_uri())
